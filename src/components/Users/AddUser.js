847c3b5213ce52fc8272419a945ae3831421f571
unknown <ch_vdw@outlook.be>
2022-03-07 16:07:06 +0100

    basic validation added

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -11,7 +11,15 @@ const AddUser = (props) => {
 
   const AddUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+        return;
+    }
+    if (+enteredAge < 1){
+        return;
+    }
     console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const userNameChangeHandler = (event) => {
@@ -25,9 +33,9 @@ const AddUser = (props) => {
     <Card className={classes.input}>
       <form onSubmit={AddUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username " type="text" onChange={userNameChangeHandler} />
+        <input id="username " type="text" value={enteredUsername} onChange={userNameChangeHandler} />
         <label htmlFor="username">Age</label>
-        <input id="username " type="number" onChange={ageChangeHandler} />
+        <input id="username " type="number" value={enteredAge} onChange={ageChangeHandler} />
         <Button type="submit">Add user</Button>
       </form>
     </Card>
