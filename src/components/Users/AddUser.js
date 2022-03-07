a28e7bdddc4f40e64f3ce14ab54adba71a744a8c
unknown <ch_vdw@outlook.be>
2022-03-07 15:08:05 +0100

    basic css styling

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,14 +1,23 @@
 import React from "react";
 
+import Card from "../UI/Card";
+
 const AddUser = (props) => {
+
+  const AddUserHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form>
-      <label htmlFor="username">Username</label>
-      <input id='username 'type="text"/>
-      <label htmlFor="username">Age</label>
-      <input id='username 'type="number"/>
-      <button type="submit">Add User</button>
-    </form>
+    <Card>
+      <form onSubmit={AddUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username " type="text" />
+        <label htmlFor="username">Age</label>
+        <input id="username " type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
