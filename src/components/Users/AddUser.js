e9a6aecc0ab37a0cdb97f6f555b9018a5e1a3f09
unknown <ch_vdw@outlook.be>
2022-03-07 15:50:19 +0100

    added state saved changes on button click

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+
+  const [enteredAge, setEnteredAge] = useState("");
+
   const AddUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername, enteredAge);
+  };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={AddUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username " type="text" />
+        <input id="username " type="text" onChange={userNameChangeHandler} />
         <label htmlFor="username">Age</label>
-        <input id="username " type="number" />
-        <Button type='submit'>Add user</Button>
+        <input id="username " type="number" onChange={ageChangeHandler} />
+        <Button type="submit">Add user</Button>
       </form>
     </Card>
   );
