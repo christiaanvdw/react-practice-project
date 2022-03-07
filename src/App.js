9b96d192e7fe69ec43a6bd8159bc5983c388a615
unknown <ch_vdw@outlook.be>
2022-03-07 19:57:32 +0100

    added key to list

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const [usersList, setUsersList] = useState([]);
 
 const addUserHandler = (uName, uAge) => {
   setUsersList((prevUsersList) => {
-    return [...prevUsersList, {name: uName, age: uAge}]; 
+    return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()}]; 
   });
 };
 
