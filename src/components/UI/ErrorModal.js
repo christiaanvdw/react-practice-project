869e232f7deb34162fe583d13827112c31158807
Christiaan van der Woude <ch_vdw@outlook.be>
2022-03-16 09:19:24 +0100

    split up errormodal component

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -4,23 +4,34 @@ import classes from "./ErrorModal.module.css";
 import Card from "./Card";
 import Button from "./Button";
 
+const Backdrop = (props) => {
+  return (
+    <div className={classes.backdrop} onClick={props.onConfirm} />
+      );
+
+  
+};
+
+const ModalOverlay = (props) => {
+  return (<Card className={classes.modal}>
+  <header className={classes.header}>
+    <h2>{props.title}</h2>
+  </header>
+  <div className={classes.content}>
+    <p>{props.message}</p>
+  </div>
+  <footer className={classes.actions}>
+    <Button onClick={props.onConfirm}>Okay</Button>
+  </footer>
+</Card>)
+};
+
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm}>
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-      </div>
-    </div>
+    <React.Fragment>
+      
+    </React.Fragment>
+    
   );
 };
 
